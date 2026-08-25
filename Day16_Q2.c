3ffab0fef135b060c5e6405fccb082548bc64365
Yash Balchandani <YASH.34541@stu.upes.ac.in>
2026-08-25 20:08:18 +0530

    Day16 Q2

---
+++ Day16_Q2.c
@@ -0,0 +1,18 @@
+// Write a program to check if a number is a palindrome.
+#include <stdio.h>
+int main() {
+    int n, original, remainder, reverse = 0;
+    scanf("%d", &n);
+    original = n;
+    while (n > 0) 
+    {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = n / 10;
+    }
+    if (original == reverse)
+        printf("Palindrome");
+    else
+        printf("Not a palindrome");
+    return 0;
+}
