5228c548781db64d87f48f968578efb8c910e8f1
Yash Balchandani <YASH.34541@stu.upes.ac.in>
2026-08-26 23:12:07 +0530

    Day17 Q1

---
+++ Day17_Q2.c
@@ -0,0 +1,21 @@
+// Write a program to check if a number is prime.
+#include <stdio.h>
+int main()
+{
+    int n, i = 1, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while(i <= n)
+    {
+        if(n % i == 0)
+        {
+            count++;
+        }
+        i++;
+    }
+    if(count == 2)
+        printf("Prime");
+    else
+        printf("Not Prime");
+    return 0;
+}
