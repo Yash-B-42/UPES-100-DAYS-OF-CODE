59822931ce3e6daaf1f455f48adda1543e4e6416
Yash Balchandani <YASH.34541@stu.upes.ac.in>
2026-08-26 23:13:23 +0530

    Day17 Q2

--- Day17_Q2.c
+++ Day17_Q2.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 int main()
 {
-    int n, i = 1, count = 0;
+    int n, i=1, count=0;
     printf("Enter a number: ");
     scanf("%d", &n);
-    while(i <= n)
+    while(i<=n)
     {
-        if(n % i == 0)
+        if(n%i==0)
         {
             count++;
         }
         i++;
     }
-    if(count == 2)
+    if(count==2)
         printf("Prime");
     else
         printf("Not Prime");
